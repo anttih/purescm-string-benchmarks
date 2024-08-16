111bc6cc92db199995920e1a3a1e5f99f46aeb5b
Antti Holvikari <antti@anttih.com>
2024-08-16 09:50:28 +0300

    Add indexOf' benchmark

--- src/Main.purs
+++ src/Main.purs
@@ -2,10 +2,13 @@ module Main where
 
 import Prelude
 
+import Data.Array.NonEmpty as NonEmpty
 import Data.Foldable (for_)
 import Data.List (List, fold, (:))
+import Data.String (Pattern(..))
 import Data.String.CodeUnits (slice)
-import Data.String.Regex (test) as Regex
+import Data.String.CodeUnits as SCU
+import Data.String.Regex (match, test) as Regex
 import Data.String.Regex.Flags as RegexFlags
 import Data.String.Regex.Unsafe (unsafeRegex) as Regex
 import Data.Unfoldable (range, replicate)
@@ -29,26 +32,33 @@ main = do
     let parts = replicate n "PureScript" :: List String
     fold parts
 
-  log "-------------------- (list) fold + slice --------------------"
+  log "-------------------- (list) fold + slice ------------"
   linear \n -> do
     let parts = replicate n "PureScript" :: List String
     slice (n * 10 / 4) (n * 10 / 4 * 3) $ fold parts
 
-  log "-------------------- (array) fold --------------------"
+  log "-------------------- (array) fold -------------------"
   linear \n -> do
     let parts = replicate n "PureScript" :: Array String
     fold parts
 
-  log "-------------------- (array) fold + slice --------------------"
+  log "-------------------- (array) fold + slice -----------"
   linear \n -> do
     let parts = replicate n "PureScript" :: Array String
     slice (n * 10 / 4) (n * 10 / 4 * 3) $ fold parts
 
-  log "-------------------- regex match --------------------"
+  log "-------------------- regex test --------------------"
   benchWith 100 \_ -> Regex.test (Regex.unsafeRegex "foo ([a-z]+) baz" RegexFlags.noFlags) "foo bar baz"
 
-  log "-------------------- regex match (precompiled) --------------------"
+  log "-------------------- regex test (precompiled) ------"
   let re = Regex.unsafeRegex "foo ([a-z]+) baz" RegexFlags.noFlags
   assert $ Regex.test re "foo bar baz"
   benchWith 100 \_ -> Regex.test re "foo bar baz"
 
+  log "-------------------- regex match -------------------"
+  let re2 = Regex.unsafeRegex "foo" RegexFlags.global
+  let input = fold (replicate 100000 "foo" :: List String)
+  benchWith 100 \_ -> NonEmpty.length <$> Regex.match re2 input
+
+  log "-------------------- CodeUnits.indexOf' ------------"
+  benchWith 100 \_ -> SCU.indexOf' (Pattern "foo") 50000 input
