b792b00fa3fc8f50bccec5d28c0964221721ca89
Antti Holvikari <antti@anttih.com>
2024-08-16 17:03:38 +0300

    Make record benchmark more realistic

--- src/Benchmark/RecordUpdate.purs
+++ src/Benchmark/RecordUpdate.purs
@@ -2,12 +2,12 @@ module Benchmark.RecordUpdate where
 
 import Prelude
 
-type R = { a :: Int }
+type R = { a :: Int, b :: Int, c :: Int, d :: Int, e :: Int, f :: Int, g :: Int, h :: Int, i :: Int }
 
 test :: Int -> R
-test n = go { a: 0 } 1
+test n = go { a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0 } 1
   where
     go :: R -> Int -> R
     go acc x = if x < n
-               then go (acc { a = x }) (x + 1)
+               then go (acc { a = x, b = x, c = x, d = x, e = x, f = x, g = x, h = x, i = x }) (x + 1)
                else acc
