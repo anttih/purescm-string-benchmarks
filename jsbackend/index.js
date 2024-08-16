c088da3f7c4d2d75e24c688a95dad81b648aeab5
Antti Holvikari <antti@anttih.com>
2024-08-16 13:53:59 +0300

    No need to pass dirname

--- jsbackend/index.js
+++ jsbackend/index.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
 import { main } from "./output/index.js";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-main(__dirname);
+main();
